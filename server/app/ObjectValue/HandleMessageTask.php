8a16901911bbe3d01b222e8b9e6c025fe0f8ce6f
rasa04 <ahuzhamberdiev@gmail.com>
2025-05-04 23:50:10 +0500

    fix

--- server/app/ObjectValue/HandleMessageTask.php
+++ server/app/ObjectValue/HandleMessageTask.php
@@ -13,12 +13,13 @@ final class HandleMessageTask implements TaskInterface
     public const string GAME_ID = 'g_id';
     public const string USER_ID = 'u_id';
     public const string BODY = 'body';
-    public const string MESSAGE_TYPE = 'm_type';
+    public const string MESSAGE_TYPE = 'type';
 
     public string $toSendViaWS
     {
         get => json_encode([
             self::BODY => $this->body,
+            'type' => $this->messageType,
             'from' => $this->userID,
             'time' => Carbon::now()->toISOString(),
         ]);
@@ -30,6 +31,7 @@ final class HandleMessageTask implements TaskInterface
             self::BODY => $this->body,
             self::GAME_ID => $this->gameID,
             self::USER_ID => $this->userID,
+            self::MESSAGE_TYPE => $this->messageType,
         ]);
     }
 
